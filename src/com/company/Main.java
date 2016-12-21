3833c23be8176d2afe341c693db50781062958ca
gevorg <gevorg.mushyan@webbfontaine.am>
2016-12-21 10:36:48 +0400

    +1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(" Git Proj 5");
+        System.out.println(" Git Proj 5+1");
     }
 }
